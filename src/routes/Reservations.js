4e1477d0f5bc7e4ac8d399b35d6b875169ea224b
Tom Eriksson <45630036+tomeriksson@users.noreply.github.com>
2023-01-09 20:10:34 +0100

    Final changes

---
+++ src/routes/Reservations.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderElement from '../components/HeaderElement';
+import Footer from '../components/Footer';
+
+const Reservations = () => {
+  return (
+    <div>
+      <HeaderElement />
+      <h1 style={{textAlign: "center", paddingTop: "15%", height: "90vh"}}>Reservations</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default Reservations
